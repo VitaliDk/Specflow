023a6dc830408e45772067983886acd6ceecfe3d
MSI\DK <danial.khan@ucl.ac.uk>
2019-10-02 23:46:22 +0100

    oksmdfs

---
+++ NUnitTestProject1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+namespace NUnitTestProject1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Url = "https://www.google.co.uk";
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            
+        }
+    }
+}
